eee177e733a2b2bf314a5f1b498dc9e02b92cc13
Melih Koçak <melihcankocakk@gmail.com>
2026-03-09 16:29:01 +0300

    feat(Application): Projede Generic Repository Pattern Kullanılacak Onun İçin Bu Commit Application Katmanında Interface Tanımlasını İçerir.

---
+++ src/Core/FonlaBeni.Application/Contracts/Persistence/IGenericRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq.Expressions;
+
+namespace FonlaBeni.Application.Contracts.Persistence;
+public interface IGenericRepository<T>
+{
+    IQueryable<T> GetAll(bool isShouldTrackChanges);
+    IQueryable<T> Where(Expression<Func<T, bool>> predicate, bool isShouldTrackChanges);
+    ValueTask<T?> GetByIdAsync(Guid id);
+    void Update(T entity);
+    void Delete(T entity);
+}
