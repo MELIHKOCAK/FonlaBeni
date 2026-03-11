44aef6d8a878eb50047344822f2156aebc31e7c1
Melih Koçak <melihcankocakk@gmail.com>
2026-03-11 10:11:05 +0300

    feat(application): Campaign Entity'si İçin Request Ve Response DTO'lar Oluşturuldu.

---
+++ src/Core/FonlaBeni.Application/DataTransferObject/Campaign/CampaignRequestDto.cs
@@ -0,0 +1,2 @@
+﻿namespace FonlaBeni.Application.DataTransferObject.Campaign;
+public record CampaignRequestDto(string Title, string Description, string Slug, decimal TargetAmount, DateTime DeadLine, Guid CategoryId, Guid UserId);
