8b8f0c83a7209a410c6a84b6cfc343ae7a63aed4
Melih Koçak <melihcankocakk@gmail.com>
2026-03-11 11:44:30 +0300

    feat(application): Payment Entity'sinin, Create Use Case'i için ve Read için  Dtolar Oluşturuldu.

---
+++ src/Core/FonlaBeni.Application/DataTransferObject/Payment/Create/PaymentRequestDto.cs
@@ -0,0 +1,5 @@
+﻿using FonlaBeni.Domain.Enums;
+
+namespace FonlaBeni.Application.DataTransferObject.Payment.Create;
+
+public record PaymentRequestDto(decimal Amount, DateTime PaymentAt, Providers ProviderName, string ProviderId, Guid DonationId, PaymentStatus Status);
---
+++ src/Core/FonlaBeni.Application/DataTransferObject/Payment/PaymentDto.cs
@@ -0,0 +1,5 @@
+﻿using FonlaBeni.Domain.Enums;
+
+namespace FonlaBeni.Application.DataTransferObject.Payment;
+
+public record PaymentDto(decimal Amount, DateTime PaymentAt, PaymentStatus Status, Providers ProviderName, string ProviderId, Guid DonationId);
