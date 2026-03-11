5382cce9870891998bd05b261b4c9076fc512e62
Melih Koçak <melihcankocakk@gmail.com>
2026-03-11 11:38:37 +0300

    feat(application): Donation Entity'sinin, Create Use Case'i için ve Read Dto Oluşturuldu.

---
+++ src/Core/FonlaBeni.Application/DataTransferObject/Donation/Create/DonationRequestDto.cs
@@ -0,0 +1,2 @@
+﻿namespace FonlaBeni.Application.DataTransferObject.Donation.Create;
+public record DonationRequestDto(decimal Amount, string Message, bool IsAnonymous, Guid CampaignId, Guid UserId);
---
+++ src/Core/FonlaBeni.Application/DataTransferObject/Donation/DonationDto.cs
@@ -0,0 +1,5 @@
+﻿using FonlaBeni.Domain.Enums;
+
+namespace FonlaBeni.Application.DataTransferObject.Donation;
+
+public record DonationDto(decimal Amount, string Message, bool IsAnonymous, PaymentStatus Status, Guid CampaignId, Guid UserId);
