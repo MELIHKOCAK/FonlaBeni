f676244e24e3c01bfaabc3b2bcbfe55f7e1cb118
Melih Koçak <melihcankocakk@gmail.com>
2026-03-12 15:55:53 +0300

    feat(application): UpdateDonationDto İçin Validation Oluşturuldu.

---
+++ src/Core/FonlaBeni.Application/Validation/UpdateDonationValidation.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using FonlaBeni.Application.DataTransferObject.Donation.Update;
+
+namespace FonlaBeni.Application.Validation;
+public class UpdateDonationValidation : AbstractValidator<UpdateDonationDto>
+{
+    public UpdateDonationValidation()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("Bağış Id'si Boş Olamaz!");
+
+        RuleFor(x => x.Status)
+          .Must(status => Enum.IsDefined(typeof(Domain.Enums.PaymentStatus), status));
+    }
+}
