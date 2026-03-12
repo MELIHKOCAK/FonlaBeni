939c0d466235ff0d5cfe1e0dcc2e539f7dc3b52e
Melih Koçak <melihcankocakk@gmail.com>
2026-03-12 16:39:15 +0300

    feat(application): CreateTagDto İçin Validation Oluşturuldu.

---
+++ src/Core/FonlaBeni.Application/Validation/CreateTagValidation.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using FonlaBeni.Application.DataTransferObject.Tag.Create;
+
+namespace FonlaBeni.Application.Validation;
+
+public class CreateTagValidation: AbstractValidator<CreateTagDto>
+{
+    public CreateTagValidation()
+    {
+        RuleFor(x => x.Name)
+            .NotNull().WithMessage("İsim Alanı Boş Olamaz.");
+    }
+}
