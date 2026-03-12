5b08e96cf8334271f3220c7a7d4ffc1b62d74f56
Melih Koçak <melihcankocakk@gmail.com>
2026-03-12 16:01:37 +0300

    feat(application): CreatePaymentDto İçin Validation Oluşturuldu.

---
+++ src/Core/FonlaBeni.Application/Validation/CreatePaymentValidation.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using FonlaBeni.Application.DataTransferObject.Payment.Create;
+
+namespace FonlaBeni.Application.Validation;
+
+public class CreatePaymentValidation : AbstractValidator<CreatePaymentDto>
+{
+    public CreatePaymentValidation()
+    {
+        RuleFor(x => x.ProviderId)
+            .NotNull().WithMessage("Sağlayıcı Id Boş Olamaz.");
+
+        RuleFor(x => x.ProviderName)
+            .Must(provider => Enum.IsDefined(typeof(Domain.Enums.Providers), provider));
+
+        RuleFor(x => x.Amount)
+            .GreaterThan(0).WithMessage("Tutar sıfırdan büyük olmalıdır.");
+
+        RuleFor(x => x.PaymentAt)
+            .LessThanOrEqualTo(DateTime.Now).WithMessage("Ödeme tarihi bugünden büyük olamaz.");
+
+        RuleFor(x => x.Status)
+          .Must(status => Enum.IsDefined(typeof(Domain.Enums.PaymentStatus), status));
+    }
+}
