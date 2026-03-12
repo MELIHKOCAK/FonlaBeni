c8a57d475ef74954cd5be0f7ec003abfe7f2f157
Melih Koçak <melihcankocakk@gmail.com>
2026-03-12 16:04:50 +0300

    feat(application): UpdatePaymentDto İçin Validation Oluşturuldu.

---
+++ src/Core/FonlaBeni.Application/Validation/UpdatePaymentValidation.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using FonlaBeni.Application.DataTransferObject.Payment.Update;
+
+namespace FonlaBeni.Application.Validation;
+public class UpdatePaymentValidation : AbstractValidator<UpdatePaymentDto>
+{
+    public UpdatePaymentValidation()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("Id Alanı Boş Geçilemez.")
+            .Must(id => Guid.TryParse(id.ToString(), out _)).WithMessage("Geçersiz Id Değeri");
+
+        RuleFor(x => x.Status)
+          .Must(status => Enum.IsDefined(typeof(Domain.Enums.PaymentStatus), status));
+    }
+}
