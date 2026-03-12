46e05d47e8a105ef79d5e3d45c5140979e6155f2
Melih Koçak <melihcankocakk@gmail.com>
2026-03-12 15:40:34 +0300

    feat(application): UpdateCategoryDto İçin Validation Oluşturuldu.

---
+++ src/Core/FonlaBeni.Application/Validation/UpdateCategoryValidation.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using FonlaBeni.Application.DataTransferObject.Category.Update;
+
+namespace FonlaBeni.Application.Validation;
+
+public class UpdateCategoryValidation : AbstractValidator<UpdateCategoryDto>
+{
+    public UpdateCategoryValidation()
+    {
+        
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Kategorinin İsmi Boş Geçilemez")
+            .MaximumLength(100).WithMessage("Kategorinin İsminin Uzunluğu 100 Karakterden Az Olmalıdır.");
+    }
+}
