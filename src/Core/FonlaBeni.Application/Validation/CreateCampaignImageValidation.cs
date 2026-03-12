7b8100a311957656d2cd2c89a48c207586678bb3
Melih Koçak <melihcankocakk@gmail.com>
2026-03-12 15:30:39 +0300

    feat(application): CreateCampaignImageDto İçin Validation Oluşturuldu.

---
+++ src/Core/FonlaBeni.Application/Validation/CreateCampaignImageValidation.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using FonlaBeni.Application.DataTransferObject.CampaignImage.Create;
+
+namespace FonlaBeni.Application.Validation;
+
+public class CreateCampaignImageValidation : AbstractValidator<CreateCampaignImageDto>
+{
+    public CreateCampaignImageValidation()
+    {
+        RuleFor(x => x.Url)
+            .NotEmpty()
+            .WithMessage("Resim URL'si boş olamaz!");
+
+        RuleFor(x => x.CampaignId)
+            .NotEmpty()
+            .WithMessage("Kampanya ID'si boş olamaz!");
+    }
+}
