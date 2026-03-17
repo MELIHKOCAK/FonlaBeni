d593d459d15bfc9fa72f4a791b9ccec94350aa91
Melih Koçak <melihcankocakk@gmail.com>
2026-03-17 22:51:55 +0300

    feat(application): UpdateCategoryValidation'a Id Kuralı Eklendi.

--- src/Core/FonlaBeni.Application/Validation/UpdateCategoryValidation.cs
+++ src/Core/FonlaBeni.Application/Validation/UpdateCategoryValidation.cs
@@ -7,7 +7,9 @@ public class UpdateCategoryValidation : AbstractValidator<UpdateCategoryDto>
 {
     public UpdateCategoryValidation()
     {
-        
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("Kategorinin Id'si Boş Geçilemez");
+
         RuleFor(x => x.Name)
             .NotEmpty().WithMessage("Kategorinin İsmi Boş Geçilemez")
             .MaximumLength(100).WithMessage("Kategorinin İsminin Uzunluğu 100 Karakterden Az Olmalıdır.");
