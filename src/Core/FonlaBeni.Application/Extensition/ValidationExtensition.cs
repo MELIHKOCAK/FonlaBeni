7befafaffee7b9e64d19cfc57e7f61c0ce1fa115
Melih Koçak <melihcankocakk@gmail.com>
2026-03-21 19:46:54 +0300

    feat(application): Validation işlemlerini yaparken tekrar eden işlemleri azaltmak için 2 adet(NotNull, NotEmpty) extensition metot oluşturuldu.

---
+++ src/Core/FonlaBeni.Application/Extensition/ValidationExtensition.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace FonlaBeni.Application.Extensition
+{
+    public static class ValidationExtensition
+    {
+        public static IRuleBuilderOptions<T, string> IsRequired<T>(this IRuleBuilder<T, string> ruleBuilder, string fieldName)
+        {
+            return ruleBuilder
+                .NotEmpty()
+                .WithMessage($"{fieldName} Alanı Boş Bırakılamaz.");
+        }
+
+        public static IRuleBuilderOptions<T, TProperty> IsRequired<T, TProperty>(this IRuleBuilder<T, TProperty> ruleBuilder, string fieldName)
+        {
+            return ruleBuilder
+                .NotNull()
+                .WithMessage($"{fieldName} Alanı Boş Bırakılamaz.");
+        }
+    }
+}
