5fbe5626ce50e12c88a45016f5b5116a9d6940a7
Melih Koçak <melihcankocakk@gmail.com>
2026-03-21 20:08:21 +0300

    refactor(application): CreateCampaignValidation Dosyası İçerisindeki Gerekli Alanlar Extensition Metot İle Yeniden Düzenlendi.

--- src/Core/FonlaBeni.Application/Campaign/Create/CreateCampaignValidation.cs
+++ src/Core/FonlaBeni.Application/Campaign/Create/CreateCampaignValidation.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using FonlaBeni.Application.Extensition;
 
 namespace FonlaBeni.Application.Campaign.Create;
 
@@ -6,29 +7,25 @@ public class CreateCampaignValidation:AbstractValidator<CreateCampaignDto>
 {
     public CreateCampaignValidation()
     {
-        RuleFor(x=> x.CategoryId)
-            .NotEmpty()
-            .WithMessage("Kategori Boş Olamaz! Lütfen Bir Kategori Seçiniz...");
+        RuleFor(x => x.CategoryId)
+            .IsRequired("Kategori Id");
 
         RuleFor(x => x.TargetAmount)
-            .NotEmpty()
-            .GreaterThan(0)
-            .WithMessage("Hedef Tutar Boş Olamaz Ve Sıfırdan Büyük Olmalıdır");
+            .IsRequired("Hedef Tutar")
+            .GreaterThan(0).WithMessage("Hedef Tutar Sıfırdan Büyük Olmalıdır");
 
         RuleFor(x => x.Description)
-            .NotEmpty()
-            .MinimumLength(300)
-            .WithMessage("Açıklama Boş Olamaz, Açıklama En Az 300 Karakter Olmalıdır");
+            .IsRequired("Açıklama")
+            .MinimumLength(300).WithMessage("Açıklama En Az 300 Karakter Olmalıdır");
 
         RuleFor(x => x.Title)
-            .MinimumLength(20)
-            .NotEmpty()
-            .MaximumLength(150)
-            .WithMessage("Başlık Boş Olamaz, Başlık 20 Karakterden Fazla Olmalı, Başlık 150 Karakterden Az Olmalı");
+            .IsRequired("Başlık")
+            .MinimumLength(20).WithMessage("Başlık En Az 20 Karakter Olmalı")
+            .MaximumLength(150).WithMessage("Başlık 150 Karakterden Az Olmalı");
 
         RuleFor(x => x.DeadLine)
-            .NotEmpty()
+            .IsRequired("Bitiş Tarihi")
             .GreaterThan(DateTime.Now)
-            .WithMessage("Kampanyanın Bitiş Tarihi Boş Olamaz Ve Bugün Olarak İşaretlenemez");
+            .WithMessage("Kampanyanın Bitiş Tarihi Bugün Olarak İşaretlenemez");
     }
 }
