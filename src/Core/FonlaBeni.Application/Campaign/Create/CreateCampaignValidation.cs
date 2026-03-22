5242050ced2dd1939fa04acec4e31ae0dd036615
Melih Koçak <melihcankocakk@gmail.com>
2026-03-22 23:55:06 +0300

    refactor(application): CreateCampaignValidation Dosyası İçerisindeki Gerekli Kurallara .Cascade() Metodu Eklendi.

--- src/Core/FonlaBeni.Application/Campaign/Create/CreateCampaignValidation.cs
+++ src/Core/FonlaBeni.Application/Campaign/Create/CreateCampaignValidation.cs
@@ -11,19 +11,23 @@ public class CreateCampaignValidation:AbstractValidator<CreateCampaignDto>
             .IsRequired("Kategori Id");
 
         RuleFor(x => x.TargetAmount)
+            .Cascade(CascadeMode.Stop)  
             .IsRequired("Hedef Tutar")
             .GreaterThan(0).WithMessage("Hedef Tutar Sıfırdan Büyük Olmalıdır");
 
         RuleFor(x => x.Description)
+            .Cascade(CascadeMode.Stop)
             .IsRequired("Açıklama")
             .MinimumLength(300).WithMessage("Açıklama En Az 300 Karakter Olmalıdır");
 
         RuleFor(x => x.Title)
+            .Cascade(CascadeMode.Stop)
             .IsRequired("Başlık")
             .MinimumLength(20).WithMessage("Başlık En Az 20 Karakter Olmalı")
             .MaximumLength(150).WithMessage("Başlık 150 Karakterden Az Olmalı");
 
         RuleFor(x => x.DeadLine)
+            .Cascade(CascadeMode.Stop)
             .IsRequired("Bitiş Tarihi")
             .GreaterThan(DateTime.Now)
             .WithMessage("Kampanyanın Bitiş Tarihi Bugün Olarak İşaretlenemez");
