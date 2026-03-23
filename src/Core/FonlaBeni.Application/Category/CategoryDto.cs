3cbc83d522f5f79a949415fa1a376b511eeba199
Melih Koçak <melihcankocakk@gmail.com>
2026-03-23 21:12:23 +0300

    feat(application): CQRS ve MEDIATOR Patterni için gerekli dosyalar oluşturuldu. Genel CategoryDto.cs Dosyası silindi.

--- src/Core/FonlaBeni.Application/Category/CategoryDto.cs
+++
@@ -1,2 +0,0 @@
-﻿namespace FonlaBeni.Application.Category;
-public record CategoryDto(string Name, string Slug, string Description);
---
+++ src/Core/FonlaBeni.Application/Category/Queries/GetAll/GetAllCategory.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace FonlaBeni.Application.Category.Queries.GetAll
+{
+    public record GetAllCategoryQueryRequest : IRequest<List<GetAllCategoryQueryResponse>>;
+
+    public class GetAllCategoryQueryHandler : IRequestHandler<GetAllCategoryQueryRequest, List<GetAllCategoryQueryResponse>>
+    {
+        public Task<List<GetAllCategoryQueryResponse>> Handle(GetAllCategoryQueryRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public record GetAllCategoryQueryResponse(string Name, string Slug, string Description);
+}
