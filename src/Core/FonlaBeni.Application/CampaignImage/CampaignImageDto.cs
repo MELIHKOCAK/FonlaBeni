631ac96308471879d4570fa6af6b65abb6ed9478
Melih Koçak <melihcankocakk@gmail.com>
2026-03-24 16:27:10 +0300

    feat(application): CQRS Pattern'i uygularken GetAllCampaignImage Feature'i İçin Gerekli Dosyalar Oluşturuldu.

--- src/Core/FonlaBeni.Application/CampaignImage/CampaignImageDto.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace FonlaBeni.Application.CampaignImage;
-public record CampaignImageDto(string Url, Guid CampaignId);
-
---
+++ src/Core/FonlaBeni.Application/CampaignImage/Queries/GetAll/GetAllCampaingImageQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace FonlaBeni.Application.CampaignImage.Queries.GetAll
+{
+    public record GetAllCampaignImageQueryRequest : IRequest<List<GetAllCampaignImageQueryResponse>>;
+
+    public class GetAllCampaignQueryHandler : IRequestHandler<GetAllCampaignImageQueryRequest, List<GetAllCampaignImageQueryResponse>>
+    {
+        public Task<List<GetAllCampaignImageQueryResponse>> Handle(GetAllCampaignImageQueryRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public record GetAllCampaignImageQueryResponse(Guid Id, string Url, Guid CampaignId);
+}
