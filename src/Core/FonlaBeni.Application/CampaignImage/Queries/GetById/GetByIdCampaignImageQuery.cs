0c7ffeec127b9027211b439a1098d94242ecdb47
Melih Koçak <melihcankocakk@gmail.com>
2026-03-24 17:03:21 +0300

    feat(application): CQRS Pattern'i uygularken GetByIdCampaignImage Feature'i İçin Gerekli Dosyalar Oluşturuldu.

---
+++ src/Core/FonlaBeni.Application/CampaignImage/Queries/GetById/GetByIdCampaignImageQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace FonlaBeni.Application.CampaignImage.Queries.GetById
+{
+    public record GetByIdCampaignImageQueryRequest(Guid Id) : IRequest<GetByIdCampaignImageQueryResponse>;
+
+    public class GetByIdCampaignQueryHandler : IRequestHandler<GetByIdCampaignImageQueryRequest, GetByIdCampaignImageQueryResponse>
+    {
+        public Task<GetByIdCampaignImageQueryResponse> Handle(GetByIdCampaignImageQueryRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public record GetByIdCampaignImageQueryResponse(Guid Id, string Url, Guid CampaignId);
+}
