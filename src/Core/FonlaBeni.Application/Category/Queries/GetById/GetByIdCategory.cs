8355a299f2eaf09e8fdbecfc5af9e26fa9ae56f1
Melih Koçak <melihcankocakk@gmail.com>
2026-03-24 14:03:34 +0300

    feat(application): CQRS Pattern'i uygularken GetByIdCategory Feature'i İçin Gerekli Dosya Oluşturuldu.

---
+++ src/Core/FonlaBeni.Application/Category/Queries/GetById/GetByIdCategory.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace FonlaBeni.Application.Category.Queries.GetById
+{
+    public record GetByIdCategoryCommandRequest(Guid Id) : IRequest<GetByIdCategoryCommandResponse>;
+
+    public class GetByIdCommandHandler : IRequestHandler<GetByIdCategoryCommandRequest, GetByIdCategoryCommandResponse>
+    {
+        public Task<GetByIdCategoryCommandResponse> Handle(GetByIdCategoryCommandRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public record GetByIdCategoryCommandResponse(Guid Id, string Name, string Slug, string Description);
+}
