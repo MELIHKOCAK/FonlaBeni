5c4da671548db73a0c35dd011eb9f5fcf077d3a3
Melih Koçak <melihcankocakk@gmail.com>
2026-03-25 19:29:09 +0300

    feat(application): CQRS Pattern'i Uygularken GetByIdDonation Feature'i İçin Dosya Oluşturuldu.

---
+++ src/Core/FonlaBeni.Application/Donation/Queries/GetById/GetByIdDonationQuery.cs
@@ -0,0 +1,17 @@
+﻿using FonlaBeni.Domain.Enums;
+using MediatR;
+
+namespace FonlaBeni.Application.Donation.Queries.GetById
+{
+    public record GetAllDonationQueryRequest : IRequest<GetAllDonationQueryResponse>;
+
+    public class GetAllDonationQueryHandler : IRequestHandler<GetAllDonationQueryRequest, GetAllDonationQueryResponse>
+    {
+        public Task<GetAllDonationQueryResponse> Handle(GetAllDonationQueryRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public record GetAllDonationQueryResponse(Guid Id, decimal Amount, string Message, bool IsAnonymous, PaymentStatus Status, Guid CampaignId, Guid UserId);
+}
