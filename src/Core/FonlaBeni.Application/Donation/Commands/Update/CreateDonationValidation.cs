8846ff1055e8b5bd21af3d32b4cb2241d01668bf
Melih Koçak <melihcankocakk@gmail.com>
2026-03-25 19:48:20 +0300

    feat(application): UpdateDonation Feature'i İçin Validasyon Dosyası Oluşturuldu.

---
+++ src/Core/FonlaBeni.Application/Donation/Commands/Update/CreateDonationValidation.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using FonlaBeni.Application.Donation.Create;
+using FonlaBeni.Application.Extensition;
+
+namespace FonlaBeni.Application.Donation.Commands.Update;
+
+public class UpdateDonationValidation : AbstractValidator<UpdateDonationCommandRequest>
+{
+    public UpdateDonationValidation()
+    {
+        RuleFor(x => x.Id)
+             .IsRequired("Id");
+    }
+}
