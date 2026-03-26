d895d79b0614ffd2d80ac923ccdf4f6be7cd6ca3
Melih Koçak <melihcankocakk@gmail.com>
2026-03-26 23:21:06 +0300

    feat(application): CreateTag Feature İçin Validasyon Kuralları Eklendi.

---
+++ src/Core/FonlaBeni.Application/Tag/Commands/Create/CreateTagValidation.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using FonlaBeni.Application.Extensition;
+
+namespace FonlaBeni.Application.Tag.Commands.Create;
+
+public class CreateTagValidation : AbstractValidator<CreateTagCommandRequest>
+{
+    public CreateTagValidation()
+    {
+        RuleFor(x => x.Name)
+            .IsRequired("İsim");
+    }
+}
