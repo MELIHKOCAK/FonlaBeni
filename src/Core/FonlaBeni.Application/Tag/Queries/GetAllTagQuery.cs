0a78c6c08d2fc39cf869cb422eb2086ffd8899fa
Melih Koçak <melihcankocakk@gmail.com>
2026-03-26 23:08:05 +0300

    feat(application): CQRS Pattern'i Uygularken GetAllTag Feature'i İçin Dosyalar Oluşturuldu.

---
+++ src/Core/FonlaBeni.Application/Tag/Queries/GetAllTagQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace FonlaBeni.Application.Tag.Queries
+{
+    public record GetAllTagQueryRequest : IRequest<List<GetAllTagQueryResponse>>;
+
+    public class GetAllTagQueryHandler : IRequestHandler<GetAllTagQueryRequest, List<GetAllTagQueryResponse>>
+    {
+        public Task<List<GetAllTagQueryResponse>> Handle(GetAllTagQueryRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public record GetAllTagQueryResponse(Guid Id, string Name, string Slug);
+}
