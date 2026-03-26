5b19c934072313a4466650be963f807cbb20052f
Melih Koçak <melihcankocakk@gmail.com>
2026-03-26 23:27:29 +0300

    feat(application): DeleteTag Feature İçin Validasyon Kuralları Eklendi.

---
+++ src/Core/FonlaBeni.Application/Tag/Commands/Delete/DeleteTagValidation.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using FonlaBeni.Application.Extensition;
+
+namespace FonlaBeni.Application.Tag.Commands.Delete;
+
+public class DeleteTagValidation : AbstractValidator<DeleteTagCommandRequest>
+{
+    public DeleteTagValidation()
+    {
+        RuleFor(x => x.Id)
+            .Cascade(CascadeMode.Stop)
+            .IsRequired("Id");
+    }
+}
+
+
