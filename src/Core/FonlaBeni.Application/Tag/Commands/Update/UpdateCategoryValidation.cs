c5751423efff6b4d05a14242a1c324836547b0ee
Melih Koçak <melihcankocakk@gmail.com>
2026-03-26 23:31:34 +0300

    feat(application): UpdateTag Feature İçin Validasyon Kuralları Eklendi.

---
+++ src/Core/FonlaBeni.Application/Tag/Commands/Update/UpdateCategoryValidation.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using FonlaBeni.Application.Extensition;
+
+namespace FonlaBeni.Application.Tag.Commands.Update;
+
+public class UpdateTagValidation : AbstractValidator<UpdateTagCommandRequest>
+{
+    public UpdateTagValidation()
+    {
+        RuleFor(x => x.Id)
+            .IsRequired("Kategori Id");
+    }
+}
