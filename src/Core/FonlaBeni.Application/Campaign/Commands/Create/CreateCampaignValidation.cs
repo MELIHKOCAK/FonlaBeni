593ebde7b39e56ee876c33f993c2136b735b0a06
Melih Koçak <melihcankocakk@gmail.com>
2026-03-27 23:16:52 +0300

    feat(application): CreateCampaign Feature'i için Validasyon Oluşturuldu.

---
+++ src/Core/FonlaBeni.Application/Campaign/Commands/Create/CreateCampaignValidation.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+using FonlaBeni.Application.Extensition;
+
+namespace FonlaBeni.Application.Campaign.Create;
+
+public class CreateCampaignValidation:AbstractValidator<CreateCampaignDto>
+{
+    public CreateCampaignValidation()
+    {
+        RuleFor(x => x.CategoryId)
+            .IsRequired("Kategori Id");
+
+        RuleFor(x => x.TargetAmount)
+            .Cascade(CascadeMode.Stop)  
+            .IsRequired("Hedef Tutar")
+            .GreaterThan(0).WithMessage("Hedef Tutar Sıfırdan Büyük Olmalıdır");
+
+        RuleFor(x => x.Description)
+            .Cascade(CascadeMode.Stop)
+            .IsRequired("Açıklama")
+            .MinimumLength(300).WithMessage("Açıklama En Az 300 Karakter Olmalıdır");
+
+        RuleFor(x => x.Title)
+            .Cascade(CascadeMode.Stop)
+            .IsRequired("Başlık")
+            .MinimumLength(20).WithMessage("Başlık En Az 20 Karakter Olmalı")
+            .MaximumLength(150).WithMessage("Başlık 150 Karakterden Az Olmalı");
+
+        RuleFor(x => x.DeadLine)
+            .Cascade(CascadeMode.Stop)
+            .IsRequired("Bitiş Tarihi")
+            .GreaterThan(DateTime.Now)
+            .WithMessage("Kampanyanın Bitiş Tarihi Bugün Olarak İşaretlenemez");
+    }
+}
