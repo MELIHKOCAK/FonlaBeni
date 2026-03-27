c84098b11f7b6f4054a59d715b7451dad71743ae
Melih Koçak <melihcankocakk@gmail.com>
2026-03-27 23:28:33 +0300

    feat(application): DeleteCampaign Feature'i için Validasyon Oluşturuldu.

---
+++ src/Core/FonlaBeni.Application/Campaign/Commands/Delete/DeleteCampaignValidation.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using FonlaBeni.Application.Extensition;
+
+namespace FonlaBeni.Application.Campaign.Commands.Delete;
+
+public class DeleteCampaignValidation : AbstractValidator<DeleteCampaignCommandRequest>
+{
+    public DeleteCampaignValidation()
+    {
+        RuleFor(x => x.Id)
+            .Cascade(CascadeMode.Stop)
+            .IsRequired("Id");
+    }
+}
+
+
