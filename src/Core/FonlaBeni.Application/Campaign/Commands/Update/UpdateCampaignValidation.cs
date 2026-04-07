cbbc2ef8a822d4651fceb4f4d427f03b71a0e629
Melih Koçak <melihcankocakk@gmail.com>
2026-04-07 11:09:08 +0300

    feat(application): UpdateCampaign Feature'i için Validasyon Oluşturuldu.

---
+++ src/Core/FonlaBeni.Application/Campaign/Commands/Update/UpdateCampaignValidation.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using FonlaBeni.Application.Campaign.Commands.Update;
+using FonlaBeni.Application.Extensition;
+
+namespace FonlaBeni.Application.Campaign.Create;
+
+public class UpdateCampaignValidation:AbstractValidator<UpdateCampaignCommandRequest>
+{
+    public UpdateCampaignValidation()
+    {
+        RuleFor(x => x.Description)
+            .Cascade(CascadeMode.Stop)
+            .IsRequired("Açıklama")
+            .MinimumLength(300).WithMessage("Açıklama En Az 300 Karakter Olmalıdır");
+
+        RuleFor(x => x.Title)
+            .Cascade(CascadeMode.Stop)
+            .IsRequired("Başlık")
+            .MinimumLength(20).WithMessage("Başlık En Az 20 Karakter Olmalı")
+            .MaximumLength(150).WithMessage("Başlık 150 Karakterden Az Olmalı");
+    }
+}
