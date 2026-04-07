c13379129e9011ef726151b622cc8ef9eb35eccd
Melih Koçak <melihcankocakk@gmail.com>
2026-04-07 11:17:12 +0300

    feat(application): CQRS Pattern'i Uygularken GetAllPayment Feature'i İçin Dosyalar Oluşturuldu.

---
+++ src/Core/FonlaBeni.Application/Payment/Queries/GetAll/GetAllPaymentQuery.cs
@@ -0,0 +1,17 @@
+﻿using FonlaBeni.Domain.Enums;
+using MediatR;
+
+namespace FonlaBeni.Application.Payment.Queries.GetAll
+{
+    public record GetAllPaymentQueryRequest : IRequest<List<GetAllPaymentQueryResponse>>;
+
+    public class GetAllPaymentQueryHandler : IRequestHandler<GetAllPaymentQueryRequest, List<GetAllPaymentQueryResponse>>
+    {
+        public Task<List<GetAllPaymentQueryResponse>> Handle(GetAllPaymentQueryRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public record GetAllPaymentQueryResponse(Guid Id,decimal Amount, DateTime PaymentAt, PaymentStatus Status, Providers ProviderName, string ProviderId, Guid DonationId);
+}
