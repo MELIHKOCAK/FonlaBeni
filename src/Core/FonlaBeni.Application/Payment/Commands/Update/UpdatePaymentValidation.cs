0f1a7a148b1ddd8ce751c8cc28ec194bd299c347
Melih Koçak <melihcankocakk@gmail.com>
2026-04-07 12:02:36 +0300

    feat(application): UpdatePayment Feature'i için Validasyon Dosyası Oluşturuldu.

---
+++ src/Core/FonlaBeni.Application/Payment/Commands/Update/UpdatePaymentValidation.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using FonlaBeni.Application.Extensition;
+
+namespace FonlaBeni.Application.Payment.Commands.Update;
+public class UpdatePaymentValidation : AbstractValidator<UpdatePaymentCommandRequest>
+{
+    public UpdatePaymentValidation()
+    {
+        RuleFor(x => x.Id)
+            .Cascade(CascadeMode.Stop)
+            .IsRequired("Id")
+            .Must(id => Guid.TryParse(id.ToString(), out _)).WithMessage("Geçersiz Id Değeri");
+
+        RuleFor(x => x.Status)
+          .Must(status => Enum.IsDefined(typeof(Domain.Enums.PaymentStatus), status));
+    }
+}
