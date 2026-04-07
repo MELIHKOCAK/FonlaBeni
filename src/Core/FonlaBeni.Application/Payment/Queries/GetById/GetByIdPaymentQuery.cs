fa15c1ce9f4d80d3a4a638ed44e19687fd9ddb01
Melih Koçak <melihcankocakk@gmail.com>
2026-04-07 11:21:57 +0300

    feat(application): CQRS Pattern'i Uygularken GetByIdPayment Feature'i İçin Dosya Oluşturuldu.

---
+++ src/Core/FonlaBeni.Application/Payment/Queries/GetById/GetByIdPaymentQuery.cs
@@ -0,0 +1,17 @@
+﻿using FonlaBeni.Domain.Enums;
+using MediatR;
+
+namespace FonlaBeni.Application.Payment.Queries.GetById
+{
+    public record GetAllPaymentQueryRequest(Guid Id) : IRequest<GetAllPaymentQueryResponse>;
+
+    public class GetAllPaymentQueryHandler : IRequestHandler<GetAllPaymentQueryRequest, GetAllPaymentQueryResponse>
+    {
+        public Task<GetAllPaymentQueryResponse> Handle(GetAllPaymentQueryRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public record GetAllPaymentQueryResponse(Guid Id, decimal Amount, DateTime PaymentAt, PaymentStatus Status, Providers ProviderName, string ProviderId, Guid DonationId);
+}
