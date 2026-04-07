77668ae8cdfdedac2e145183b0c0977ebd373f4b
Melih Koçak <melihcankocakk@gmail.com>
2026-04-07 10:51:58 +0300

    feat(application): CQRS Pattern'i Uygularken GetAllCampaign Feature'i İçin Dosyalar Oluşturuldu.

---
+++ src/Core/FonlaBeni.Application/Campaign/Queries/GetAll/GetAllCampaignQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace FonlaBeni.Application.Campaign.Queries.GetAll
+{
+    public record GetAllCampaignQueryRequest : IRequest<List<GetAllCampaignQueryResponse>>;
+
+    public class GetAllCampaignQueryHandler : IRequestHandler<GetAllCampaignQueryRequest, List<GetAllCampaignQueryResponse>>
+    {
+        public Task<List<GetAllCampaignQueryResponse>> Handle(GetAllCampaignQueryRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public record GetAllCampaignQueryResponse(Guid Id, string Name, string Slug);
+}
