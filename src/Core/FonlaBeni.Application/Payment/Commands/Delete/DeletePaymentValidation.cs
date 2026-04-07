f2926de3171423d9a07e8abe1dc5a13ce154fe9e
Melih Koçak <melihcankocakk@gmail.com>
2026-04-07 11:56:49 +0300

    feat(application): DeletePayment Feature'i için Validasyon Dosyası Oluşturuldu.

---
+++ src/Core/FonlaBeni.Application/Payment/Commands/Delete/DeletePaymentValidation.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using FonlaBeni.Application.Extensition;
+
+namespace FonlaBeni.Application.Payment.Commands.Delete;
+
+public class DeletePaymentValidation : AbstractValidator<DeletePaymentCommandRequest>
+{
+    public DeletePaymentValidation()
+    {
+        RuleFor(x => x.Id)
+            .Cascade(CascadeMode.Stop)
+            .IsRequired("Id");
+    }
+}
+
+
