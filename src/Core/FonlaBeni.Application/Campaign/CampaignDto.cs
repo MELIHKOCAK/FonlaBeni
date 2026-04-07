2b6f97ffd6684fcad35883113c7941fa3d083de1
Melih Koçak <melihcankocakk@gmail.com>
2026-04-07 10:56:51 +0300

    refactor(application): CQRS pattern'i uygulandıktan sonra kullanılmayan dosyalar silindi.

--- src/Core/FonlaBeni.Application/Campaign/CampaignDto.cs
+++
@@ -1,4 +0,0 @@
-﻿using FonlaBeni.Domain.Enums;
-
-namespace FonlaBeni.Application.Campaign;
-public record CampaignDto(string Title, string Description, string Slug, decimal TargetAmount, decimal CurrentAmount, int BackersCount, DateTime DeadLine, CampaignStatus Status);
