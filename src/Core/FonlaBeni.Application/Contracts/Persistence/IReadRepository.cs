7927d334f8c0a0054f99989da4e01f9ee9777366
Melih Koçak <melihcankocakk@gmail.com>
2026-04-09 13:12:40 +0300

    feat(application): CQRS Pattern'i İçin IReadRepository Dosyası Oluşturuldu.

---
+++ src/Core/FonlaBeni.Application/Contracts/Persistence/IReadRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace FonlaBeni.Application.Contracts.Persistence
+{
+    public interface IReadRepository<T>
+    {
+        IQueryable<T> GetAll(bool isShouldTrackChanges);
+        IQueryable<T> Where(Expression<Func<T, bool>> predicate, bool isShouldTrackChanges);
+        ValueTask<T?> GetByIdAsync(Guid id);
+    }
+
+}
