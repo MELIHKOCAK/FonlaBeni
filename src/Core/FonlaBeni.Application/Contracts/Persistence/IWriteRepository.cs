d6cff5a9bbb07e1c616db9f36375f2298e5ee682
Melih Koçak <melihcankocakk@gmail.com>
2026-04-09 13:13:28 +0300

    feat(application): CQRS Pattern'i İçin IWriteRepository Dosyası Oluşturuldu.

---
+++ src/Core/FonlaBeni.Application/Contracts/Persistence/IWriteRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace FonlaBeni.Application.Contracts.Persistence
+{
+    public interface IWriteRepository<T>
+    {
+        ValueTask AddAsync(T Entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
