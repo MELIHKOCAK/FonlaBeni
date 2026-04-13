05d6b6c971751206a736447a7e5e95ab5240c486
Melih Koçak <melihcankocakk@gmail.com>
2026-04-13 15:46:32 +0300

    feat(application): UpdateUser Feature'i için Validasyon Oluşturuldu.

---
+++ src/Core/FonlaBeni.Application/AppUser/Commands/Update/UpdateCategoryValidation.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using FonlaBeni.Application.Extensition;
+
+namespace FonlaBeni.Application.AppUser.Commands.Update;
+
+public class UpdateUserValidation : AbstractValidator<UpdateUserCommandRequest>
+{
+    public UpdateUserValidation()
+    {
+        RuleFor(x => x.Id)
+            .IsRequired("Id");
+
+        RuleFor(x => x.Name)
+            .Cascade(CascadeMode.Stop)
+            .IsRequired("İsim");
+
+        RuleFor(x => x.Surname)
+            .Cascade(CascadeMode.Stop)
+            .IsRequired("Soyisim");
+
+        RuleFor(x => x.ProfileImageUrl)
+            .Cascade(CascadeMode.Stop)
+            .IsRequired("Profil Resmi");
+        RuleFor(x => x.PhoneNumber)
+            .Cascade(CascadeMode.Stop)
+            .IsRequired("Telefon Numarası");
+    }
+}
