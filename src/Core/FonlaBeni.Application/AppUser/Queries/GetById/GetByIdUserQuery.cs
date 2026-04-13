cebd8d51a822197fa51b782365eadd9f4e8d7b56
Melih Koçak <melihcankocakk@gmail.com>
2026-04-13 15:01:45 +0300

    feat(application): CQRS Pattern'i Uygularken GetByIdUser Feature'i İçin Dosya Oluşturuldu.

---
+++ src/Core/FonlaBeni.Application/AppUser/Queries/GetById/GetByIdUserQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace FonlaBeni.Application.AppUser.Queries.GetById
+{
+    public record GetByIdUserQueryRequest(Guid Id) : IRequest<GetByIdUserQueryResponse>;
+
+    public class GetByIdUserQueryHandler : IRequestHandler<GetByIdUserQueryRequest, GetByIdUserQueryResponse>
+    {
+        public Task<GetByIdUserQueryResponse> Handle(GetByIdUserQueryRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public record GetByIdUserQueryResponse(Guid Id, string Name, string Surname, string ProfileImageUrl, string UserName, string NormalizedUserName, string Email, string NormalizedEmail, string PhoneNumber, bool TwoFactorEnabled);
+}
