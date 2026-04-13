a81da5fbdce9469a8a0815f2a35c466ba5cc2398
Melih Koçak <melihcankocakk@gmail.com>
2026-04-13 14:56:13 +0300

    feat(application): CQRS Pattern'i Uygularken GetAllUser Feature'i İçin Dosya Oluşturuldu.

---
+++ src/Core/FonlaBeni.Application/AppUser/Queries/GetAll/GetAllUserQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace FonlaBeni.Application.AppUser.Queries.GetAll
+{
+    public record GetAllUserQueryRequest : IRequest<List<GetAllUserQueryResponse>>;
+
+    public class GetAllUserQueryHandler : IRequestHandler<GetAllUserQueryRequest, List<GetAllUserQueryResponse>>
+    {
+        public Task<List<GetAllUserQueryResponse>> Handle(GetAllUserQueryRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public record GetAllUserQueryResponse(Guid Id, string Name, string Surname, string ProfileImageUrl, string UserName, string NormalizedUserName, string Email, string NormalizedEmail, string PhoneNumber, bool TwoFactorEnabled);
+}
