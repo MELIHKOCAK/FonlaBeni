a00a9e235717b08c52fcbabeee260278d80b484b
Melih Koçak <melihcankocakk@gmail.com>
2026-04-13 15:32:46 +0300

    feat(application): DeleteUser Feature'i için Validasyon Oluşturuldu.

---
+++ src/Core/FonlaBeni.Application/AppUser/Commands/Delete/DeleteUserValidation.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using FonlaBeni.Application.Extensition;
+
+namespace FonlaBeni.Application.Category.Commands.DeleteCategory;
+
+public class DeleteUserValidation : AbstractValidator<DeleteCategoryCommandRequest>
+{
+    public DeleteUserValidation()
+    {
+        RuleFor(x => x.Id)
+            .Cascade(CascadeMode.Stop)
+            .IsRequired("Id");
+    }
+}
+
+
