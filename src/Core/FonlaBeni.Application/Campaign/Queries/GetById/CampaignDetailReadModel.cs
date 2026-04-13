6e33a4e04220aa92950dea83092fbff024f668dc
Melih Koçak <melihcankocakk@gmail.com>
2026-04-13 11:40:33 +0300

    feat(application): CQRS pattern'i uygularken ReadDatabase İçin CampaignDetailReadModel dosyası oluşturuldu.

---
+++ src/Core/FonlaBeni.Application/Campaign/Queries/GetById/CampaignDetailReadModel.cs
@@ -0,0 +1,20 @@
+﻿using FonlaBeni.Domain.Enums;
+
+namespace FonlaBeni.Application.Campaign.Queries.GetById
+{
+    public class CampaignDetailReadModel
+    {
+        public Guid Id { get; init; }
+        public string CampaignName { get; init; }
+        public string CampaignDescription { get; init; }
+        public decimal TargetAmount { get; init; }
+        public decimal CurrentAmount { get; init; }
+        public string CategoryName { get; init; }
+        public DateTime DeadLine { get; init; }
+        public DateTime CreatedDate { get; init; }
+        public string TagName { get; init; }
+        public CampaignStatus Status { get; init; }
+        public PaymentStatus PayoutStatus { get; init; }
+        public List<string> ImageUrl { get; init; }
+    }
+}
