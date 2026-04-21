684796bf75a05d59471244eb011f21986fb8e56a
Melih Koçak <melihcankocakk@gmail.com>
2026-04-21 20:30:02 +0300

    feat(Persistence): Veri Tabanı Bağlantı Dizilerine Erişmek İçin Options Pattern Tercih Edildi Ve Gerekli Dosyalar Düzenlenip Oluşturuldu.

---
+++ src/Infrastructure/FonlaBeni.Persistence/Options/DbOptions.cs
@@ -0,0 +1,16 @@
+﻿namespace FonlaBeni.Persistence.Options
+{
+    public class DbOptions
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string Database { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        override public string ToString()
+        {
+            return $"Host={Host};Database={Database};Port={Port};Username={Username};Password={Password}";
+        }
+    }
+}
---
+++ src/Infrastructure/FonlaBeni.Persistence/Options/PostgreSqlOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace FonlaBeni.Persistence.Options
+{
+    public class PostgreSqlOptions
+    {
+        public DbOptions WriteDb { get; set; }
+        public DbOptions ReadDb { get; set; }
+    }
+}
